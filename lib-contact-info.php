a3e69a06e8486d60dbe66bc37435f85df399d3c6
David Baker <dbaker3@gmail.com>
2018-02-08 09:45:31 -0500

    Added mail icon

--- lib-contact-info.php
+++ lib-contact-info.php
@@ -36,7 +36,6 @@ class ContactWidget extends WP_Widget
 	$content .= '<span itemprop="addressLocality">Milligan College</span>, <span itemprop="addressRegion">TN</span> <span itemprop="postalCode">37682</span><br />';
 	$content .= '<span itemprop="telephone">423.461.8703</span> (Welshimer)<br />';
 	$content .= '<span itemprop="telephone">423.461.1540</span> (Seminary)<br />';
-	$content .= '<a itemprop="email" href="mailto:library@milligan.edu">library@milligan.edu</a>';
 	$content .= '</div>';
  	$content .= '<div class="sidebar-social-menu">';
 	$content .= '<ul>';
@@ -44,6 +43,7 @@ class ContactWidget extends WP_Widget
 	$content .=	'<li><a href="https://twitter.com/intent/user?screen_name=MilliganLibrary" target="_blank"></a></li><!--Twitter-->';
 	$content .=	'<li><a href="https://www.facebook.com/milligancollegelibrary" target="_blank"></a></li><!--Facebook-->';
 	$content .=	'<li><a href="https://www.instagram.com/milliganlibraries/" target="_blank"></a></li>';
+	$content .=	'<li><a href="mailto:library@milligan.edu"></a></li>';
    $content .= '</ul>';
 	$content .=	'</div>';
  	echo $content;
